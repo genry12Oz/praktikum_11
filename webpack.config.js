4368823ba1254876eb05153030e19cf38f1e762d
Sergey <F.J.Fray@yandex.ru>
2020-05-30 13:51:07 +0300

    Настроена сборка build и dev

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+// подключаем утилиту относительного пути
+const path = require('path');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    }
+}
