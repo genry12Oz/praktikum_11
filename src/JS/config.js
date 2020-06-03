694aec7a9b0e3e40b00579c1b9db73e922616eee
Sergey <F.J.Fray@yandex.ru>
2020-06-03 19:44:29 +0300

    Updates

--- src/JS/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    cardsUrl: (NODE_ENV === 'development') ? 'http://praktikum.tk/cohort9/cards' : 'https://praktikum.tk/cohort9/cards',
-    infoUrl: (NODE_ENV === 'development') ? 'http://praktikum.tk/cohort9/users/me': 'https://praktikum.tk/cohort9/users/me',
-    options: {
-        headers: {
-            authorization: 'cbb262b1-dd93-4685-a69f-8e082b08e5ce'
-          }
-    }
-}
